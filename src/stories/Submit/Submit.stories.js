ca01027e8bd04d775d8bf5487ec1d2a13afcb371
Roger Kondrat <misterhtmlcss@gmail.com>
2020-09-24 13:57:03 -0600

    trying to figure out how to get Stories to bundle Input withing itself and then button eventually

--- src/stories/Submit/Submit.stories.js
+++ src/stories/Submit/Submit.stories.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import * as InputStories from '../Input/Input.stories'
+import { Input } from '../Input/Input.stories'
+// import { Default } from '../Input/Input.stories'
+// import * as InputStories from '../Input/Input.stories'
+// console.log("InputStories", InputStories);
+
+
 
 export default {
   title: "Form/Submit",
@@ -8,7 +13,9 @@ export default {
 
 export const Submit = () => (
   <>
-      <InputStories.Default />
+    <Input />
+    {/* <Default /> */}
+    {/* <InputStories /> */}
   </>
 )
 
